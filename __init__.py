de42f0feeb1e1aedf426f2f1b03456779725b424
JarbasAi <jarbasai@mailfence.com>
2023-12-09 16:39:28 +0000

    modernize imports

--- __init__.py
+++ __init__.py
@@ -12,42 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_handler
-from mycroft.audio import wait_while_speaking
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
 
 
-class SpellingSkill(MycroftSkill):
-    SEC_PER_LETTER = 0.9         # based on the Mark 1 scrolling speed
-    LETTERS_PER_SCREEN = 7.0     # based on the Mark 1 screen size
-
-    def __init__(self):
-        super(SpellingSkill, self).__init__(name="SpellingSkill")
+class SpellingSkill(OVOSSkill):
+    SEC_PER_LETTER = 0.9  # based on the Mark 1 scrolling speed
+    LETTERS_PER_SCREEN = 7.0  # based on the Mark 1 screen size
 
     @intent_handler(IntentBuilder("").require("Spell").require("Word"))
     def handle_spell(self, message):
         word = message.data.get("Word")
         spelled_word = '; '.join(word).upper()
 
-        self.enclosure.deactivate_mouth_events()
-        self.speak(spelled_word)
-
         # Pause mouth shapes appearing on screen for at least enough time
         # for the word to scroll by on the Mark 1 screen.  Pad with blanks
         # to prevent re-starting the scroll action if the timing is slightly
         # off.
+        self.enclosure.deactivate_mouth_events()
+        self.enclosure.mouth_text(word + "          ")
 
-        # TODO: Add mouth_text(word, wrap_at_end=False) parameter and get rid
-        #       of the need for deactivate_mouth_events() -- or at least handle
-        #       at the Enclosure level.
-        self.enclosure.mouth_text(word+"          ")
-        time.sleep(self.LETTERS_PER_SCREEN + len(word) * self.SEC_PER_LETTER)
-        wait_while_speaking()
+        self.speak(spelled_word, wait=True)
 
+        # allow mouth movements again
         self.enclosure.activate_mouth_events()
         self.enclosure.mouth_reset()
-
-
-def create_skill():
-    return SpellingSkill()
